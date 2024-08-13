99712c01648a13a82b37a179fc1228bd7ec1a7fa
Srinjoy Sen Chowdhury <116475469+AllMightLegend@users.noreply.github.com>
2024-08-13 23:58:34 +0530

    frontend

---
+++ frontend/src/components/features24.js
@@ -0,0 +1,162 @@
+import React, { useState, Fragment } from 'react'
+
+import PropTypes from 'prop-types'
+
+import './features24.css'
+
+const Features24 = (props) => {
+  const [activeTab, setActiveTab] = useState(0)
+  return (
+    <div className="thq-section-padding">
+      <div className="features24-container2 thq-section-max-width">
+        <div className="features24-image-container">
+          {activeTab === 0 && (
+            <img
+              alt={props.feature1ImgAlt}
+              src={props.feature1ImgSrc}
+              className="features24-image1 thq-img-ratio-16-9"
+            />
+          )}
+          {activeTab === 1 && (
+            <img
+              alt={props.feature2ImgAlt}
+              src={props.feature2ImgSrc}
+              className="features24-image2 thq-img-ratio-16-9"
+            />
+          )}
+          {activeTab === 2 && (
+            <img
+              alt={props.feature3ImgAlt}
+              src={props.feature3ImgSrc}
+              className="features24-image3 thq-img-ratio-16-9"
+            />
+          )}
+        </div>
+        <div className="features24-tabs-menu">
+          <div
+            onClick={() => setActiveTab(0)}
+            className="features24-tab-horizontal1"
+          >
+            <div className="features24-divider-container1">
+              {activeTab === 0 && <div className="features24-container3"></div>}
+            </div>
+            <div className="features24-content1">
+              <h2>
+                {props.feature1Title ?? (
+                  <Fragment>
+                    <h2 className="features24-text4 thq-heading-2">
+                      Wallet Connectivity
+                    </h2>
+                  </Fragment>
+                )}
+              </h2>
+              <span>
+                {props.feature1Description ?? (
+                  <Fragment>
+                    <span className="features24-text5 thq-body-small">
+                      Seamlessly connect to various cryptocurrency wallets for
+                      easy transactions.
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+          <div
+            onClick={() => setActiveTab(1)}
+            className="features24-tab-horizontal2"
+          >
+            <div className="features24-divider-container2">
+              {activeTab === 1 && <div className="features24-container4"></div>}
+            </div>
+            <div className="features24-content2">
+              <h2>
+                {props.feature2Title ?? (
+                  <Fragment>
+                    <h2 className="features24-text6 thq-heading-2">
+                      Token Swapping
+                    </h2>
+                  </Fragment>
+                )}
+              </h2>
+              <span>
+                {props.feature2Description ?? (
+                  <Fragment>
+                    <span className="features24-text3 thq-body-small">
+                      Effortlessly swap different cryptocurrencies with a
+                      user-friendly interface.
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+          <div
+            onClick={() => setActiveTab(2)}
+            className="features24-tab-horizontal3"
+          >
+            <div className="features24-divider-container3">
+              {activeTab === 2 && <div className="features24-container5"></div>}
+            </div>
+            <div className="features24-content3">
+              <h2>
+                {props.feature3Title ?? (
+                  <Fragment>
+                    <h2 className="features24-text2 thq-heading-2">
+                      Real-time Price Visualization
+                    </h2>
+                  </Fragment>
+                )}
+              </h2>
+              <span>
+                {props.feature3Description ?? (
+                  <Fragment>
+                    <span className="features24-text1 thq-body-small">
+                      View real-time updates of cryptocurrency prices with
+                      interactive charts and graphs.
+                    </span>
+                  </Fragment>
+                )}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+Features24.defaultProps = {
+  feature1ImgAlt: 'Wallet Connectivity Icon',
+  feature3Description: undefined,
+  feature3Title: undefined,
+  feature3ImgSrc:
+    'https://images.unsplash.com/photo-1642354984104-90e16a092c47?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyMzQ3OTgyMXw&ixlib=rb-4.0.3&q=80&w=1080',
+  feature1ImgSrc:
+    'https://images.unsplash.com/photo-1666944073140-6bb0f0c5209e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyMzQ3OTgyMnw&ixlib=rb-4.0.3&q=80&w=1080',
+  feature2Description: undefined,
+  feature1Title: undefined,
+  feature3ImgAlt: 'Real-time Price Visualization Icon',
+  feature1Description: undefined,
+  feature2ImgSrc:
+    'https://images.unsplash.com/photo-1676907820177-6784c3a05bf7?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTcyMzQ3OTgyMnw&ixlib=rb-4.0.3&q=80&w=1080',
+  feature2ImgAlt: 'Token Swapping Icon',
+  feature2Title: undefined,
+}
+
+Features24.propTypes = {
+  feature1ImgAlt: PropTypes.string,
+  feature3Description: PropTypes.element,
+  feature3Title: PropTypes.element,
+  feature3ImgSrc: PropTypes.string,
+  feature1ImgSrc: PropTypes.string,
+  feature2Description: PropTypes.element,
+  feature1Title: PropTypes.element,
+  feature3ImgAlt: PropTypes.string,
+  feature1Description: PropTypes.element,
+  feature2ImgSrc: PropTypes.string,
+  feature2ImgAlt: PropTypes.string,
+  feature2Title: PropTypes.element,
+}
+
+export default Features24
